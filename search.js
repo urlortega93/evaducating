78efda0b96231c4aed77bc5ceb47c630aeb32e2b
urlortega93 <uriel.ortega@uabc.edu.mx>
2019-12-02 14:50:48 -0800

    MFC

---
+++ search.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+ 
+    (function ($) {
+ 
+        $('#filtrar').keyup(function () {
+ 
+             var rex = new RegExp($(this).val(), 'i');
+ 
+             $('.buscar tr').hide();
+ 
+             $('.buscar tr').filter(function () {
+               return rex.test($(this).text());
+             }).show();
+ 
+        })
+ 
+    }(jQuery));
+ 
+});
